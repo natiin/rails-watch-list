9a4d09b9350c7d8456eca63f7cfa7e900c3c8c97
nt <nat.targowska@gmail.com>
2021-08-20 15:08:28 +0100

    app

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+require 'pry-byebug'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts 'start seeding...'
+
+index = 0
+Movie.destroy_all
+Bookmark.destroy_all
+
+10.times do
+  movie = Movie.create!(
+    title: movies['results'][index]['title'],
+    overview: movies['results'][index]['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w300#{movies['results'][index]['poster_path']}",
+    rating: movies['results'][index]['vote_average']
+  )
+  movie.poster_url = "https://image.tmdb.org/t/p/w300#{movies['results'][index]['poster_path']}"
+  movie.save!
+  # puts movie.poster_url
+  index += 1
+end
+
+puts 'end seeding...'
